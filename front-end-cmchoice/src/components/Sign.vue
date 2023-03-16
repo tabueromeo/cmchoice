c9dfe43986b2c3a69a84af94d63a591274b67c17
unknown <corine.ngueket@gmail.com>
2023-03-16 13:06:12 +0100

    Front signup

---
+++ front-end-cmchoice/src/components/Sign.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="container">
+    <h3>Inscription</h3>
+<br>
+    <!-- <div class="contain-form"> -->
+      <form action="#" method="post">
+        <div class="form-group">
+          <span><img src="../assets/User.svg" alt="user icon"></span>
+          <input type="text" placeholder="username" class="form-field">
+        </div>
+        <div class="form-group">
+          <span><img src="../assets/User.svg" alt="user icon"></span>
+          <input type="text" placeholder="email" class="form-field">
+        </div>
+        <div class="form-group">
+          <span><img src="../assets/User.svg" alt="user icon"></span>
+          <input type="password" placeholder="Password" class="form-field">
+        </div>
+        <div class="form-group">
+          <span><img src="../assets/User.svg" alt="user icon"></span>
+          <input type="password" placeholder="Confirm password" class="form-field">
+        </div>
+        <div class="form-group">
+          <span><img src="../assets/User.svg" alt="user icon"></span>
+          <input type="telephone" placeholder="Contact" class="form-field">
+        </div>
+        <div class="form-group">
+          <span><img src="../assets/User.svg" alt="user icon"></span>
+          <input type="text" placeholder="Domaine d'activité" class="form-field">
+        </div>
+        <div class="form-group">
+          <span><img src="../assets/User.svg" alt="user icon"></span>
+          <input type="text" placeholder="Nom de la société" class="form-field">
+        </div>
+        <div class="form-group">
+          <span><img src="../assets/User.svg" alt="user icon"></span>
+          <input type="text" placeholder="Etat civil" class="form-field">
+        </div>
+        <div class="form-check">
+          <div class="ckeck">
+            <input type="checkbox" name="Producteur" id="Producteur"><label for="#">Client</label>
+          </div>
+          <div class="ckeck">
+            <input type="checkbox" name="Producteur" id="Producteur"><label for="#">Vendeur</label>
+          </div>
+        </div>
+
+        <div class="form-btn">
+          <button>Soumettre</button>
+        </div>
+      </form>
+    <!-- </div> -->
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+  .container{
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+  }
+  /* form .form-group, .form-check{
+    max-width: 360px;
+  } */
+  form .form-group:not(:last-child){
+    margin-bottom: 10px;
+  }
+
+  form{
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    grid-area: form;
+  }
+  .user{
+    display: flex;
+    border-bottom: 1px solid black;
+  }
+  .form-group{
+    position: relative;
+    display: flex;
+    width: 100%;
+    border-bottom: 1px solid #000000;
+    flex: 1;
+  }
+  span{
+    position: absolute;
+    left: 0;
+    top: -1px;
+  }
+  .form-group > span, .form-field{
+    border-radius: 0;
+  }
+  input{
+    border: none;
+    padding: 10px 0  0 35px;
+    font-size: 1rem;
+    outline: none;
+    flex: 1;
+  }
+  input::placeholder{
+    opacity: 0.5;
+  }
+  input:hover{
+    border-bottom: 1px solid red;
+  }
+  .form-check{
+    display: flex;
+    justify-content: space-between;
+    /* flex-direction: column; */
+  }
+  
+</style>
---
+++ front-end-cmchoice/src/views/LoginView.vue
@@ -0,0 +1,43 @@
+<script setup>
+import Sign from '../components/Sign.vue';
+
+    // components: { Sign}
+</script>
+
+<template>
+    <div class="container-fluid">
+        
+        <div class="vector">
+            <!-- <img src="../assets/Vector.svg" alt="vecteur"> -->
+        </div>
+        <div>
+            <Sign/>
+        </div>
+    </div>
+   
+    
+</template>
+
+
+<style>
+    .container-fluid{
+        margin: 10px;
+        background: #FFFFFF;
+        border-radius: 10px;   
+        box-shadow: 1px 4px 4px #847676;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: 1fr;
+        grid-template-areas: 'vector' 'form';
+        width: 98%;
+        height: 615px;
+    }
+    .vector{
+        flex: 1;
+        grid-area: vector;
+        background-image: url(../assets/Vecto.png);
+        background-size: contain;
+        background-repeat: no-repeat;
+        width: 100%;
+    }
+</style>
