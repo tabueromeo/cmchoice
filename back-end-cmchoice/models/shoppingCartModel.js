2ad4492e929957141980b410c2462b6a6636913a
tchopcoin manger <tchopcoininfos@gmail.com>
2023-05-23 13:53:51 +0200

    review de architecture

---
+++ back-end-cmchoice/models/shoppingCartModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let ShoppingCart = new Schema(
+	{
+		userId: {
+			type: mongoose.Types.ObjectId,
+			ref: "users",
+			required: [true, "user id is required"],
+		},
+		products: [
+			{
+				_id: false,
+				item: { type: mongoose.Types.ObjectId, ref: "Products" },
+				quantity: Number,
+			},
+		],
+		commands: [
+			{
+				_id: false,
+				command: { type: mongoose.Types.ObjectId, ref: "Commands" },
+			},
+		],
+	},
+	{
+		timestamps: true,
+	}
+);
+module.exports = mongoose.model("ShoppingCarts", ShoppingCart);
--- back-end-cmchoice/routes/subCategory.js
+++ back-end-cmchoice/routes/subCategory.js
@@ -4,26 +4,34 @@ const express = require("express");
 // We use it to define our routes.
 // The router will be added as a middleware and will take control of requests starting with path /subCategory.
 const subCategoryRoutes = express.Router();
- 
+
 // import the subCategory controller
-let SubCategoryControls = require('../controllers/subCategory.controller');
+let SubCategoryControls = require("../controllers/subCategoryController");
 
 // import the authentification middleware
-const authMiddleWare = require('../middleWare/authMiddleWare')
- 
+const authMiddleWare = require("../middleWare/authMiddleWare");
+
 // This section will help you get a list of all the subCategorys.
 subCategoryRoutes.route("/api/v1/subCategories").get(SubCategoryControls.all);
 
 // This section will help you create a subCategorys.
-subCategoryRoutes.route("/api/v1/subCategories").post(authMiddleWare, SubCategoryControls.createSubCategory);
- 
+subCategoryRoutes
+	.route("/api/v1/subCategories")
+	.post(authMiddleWare, SubCategoryControls.createSubCategory);
+
 // This section will help you get a single subCategory by id
-subCategoryRoutes.route("/api/v1/subCategories/:id").get(SubCategoryControls.getSubCategory);
- 
+subCategoryRoutes
+	.route("/api/v1/subCategories/:id")
+	.get(SubCategoryControls.getSubCategory);
+
 // This section will help you update a subCategory by id.
-subCategoryRoutes.route("/api/v1/subCategories/:id").put(authMiddleWare, SubCategoryControls.updateSubCategory);
- 
+subCategoryRoutes
+	.route("/api/v1/subCategories/:id")
+	.put(authMiddleWare, SubCategoryControls.updateSubCategory);
+
 // This section will help you delete a subCategory
-subCategoryRoutes.route("/api/v1/subCategories/:id").delete(authMiddleWare, SubCategoryControls.deleteSubCategory);
- 
+subCategoryRoutes
+	.route("/api/v1/subCategories/:id")
+	.delete(authMiddleWare, SubCategoryControls.deleteSubCategory);
+
 module.exports = subCategoryRoutes;
